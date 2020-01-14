baa1eedb1bdcee9e6675db722266e1c14a359470
lucaspauloop <lucaspaulodeoliveira@outlook.com>
2020-01-14 17:51:18 -0300

    update backend adicionado funções para delete e update

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -4,32 +4,63 @@ const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
 
-    async getAllUser(request,response) {
-        const users = await User.find();
-        response.json(users);
-    },
-    
-    async registerUser(request, response) {
-        const { github_username, techs, latitude, longitude } = request.body;
-        const getUser = await User.findOne({github_username},{_id:0})
-        if(getUser)
-        {
-            response.json({status: 400,message: "Usuário já cadastrado"})
-        }
-        else if(!getUser){
-            const techsArray = parseStringAsArray(techs);
-            const responseGithub = await axios.get(`https://api.github.com/users/${github_username}`);
-            const { name = login, avatar_url, bio } = responseGithub.data
-            const location = { type: "Point", coordinates: [longitude, latitude] }
-            const user = await User.create({
-                github_username,
-                name,
-                avatar_url,
-                bio,
-                techs: techsArray,
-                location
-            })
-            return response.json(user);
-        }
-    }
+	async getAllUser(request, response) {
+		const users = await User.find();
+		response.json(users);
+	},
+
+	async registerUser(request, response) {
+		const { github_username, techs, latitude, longitude } = request.body;
+		const getUser = await User.findOne({ github_username }, { _id: 0 })
+		if (getUser) {
+			response.json({ status: 400, message: "Usuário já cadastrado" })
+		}
+		else if (!getUser) {
+			const techsArray = parseStringAsArray(techs);
+			const responseGithub = await axios.get(`https://api.github.com/users/${github_username}`);
+			const { name = login, avatar_url, bio } = responseGithub.data
+			const location = { type: "Point", coordinates: [longitude, latitude] }
+			const user = await User.create({
+				github_username,
+				name,
+				avatar_url,
+				bio,
+				techs: techsArray,
+				location
+			})
+			return response.json(user);
+		}
+	},
+
+	async updateUser(request, response) {
+		const { github_username, name, avatar_url, bio, techs } = request.body
+		const user = await User.find({ github_username }, { _id: 0 });
+		if (user) {
+			techs = await parseStringAsArray(techs);
+			await User.create({
+				...user,
+				github_username : github_username || user.github_username,
+				name : name || user.name,
+				avatar_url : avatar_url || user.avatar_url,
+				bio : bio || user.bio,
+				techs : techs || user.techs
+			})
+			return response.json(user);
+		}
+		else if (!user) {
+			return response.json({ status: 404, message: "Usuário não encontrado" })
+		}
+
+	},
+
+	async deleteUser(request, response) {
+		const { github_username } = request.params
+		const deleteInformation = await User.deleteOne({ github_username });
+		if(!deleteInformation.deletedCount)
+			return response.json({status:403, message:"Não possui usuário com este github username"})
+
+		if(deleteInformation.deletedCount)
+			return response.json({status: 200, message:"Deletado com sucesso"});
+	}
+
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,13 @@ const routes = Router();
 const userControllers = require("./controllers/user");
 const searchUserControllers = require("./controllers/searchUser");
 
+//users
 routes.post("/users",userControllers.registerUser)
 routes.get("/users",userControllers.getAllUser);
+routes.put("/users",userControllers.updateUser);
+routes.delete("/users/:github_username",userControllers.deleteUser);
+
+//search
 routes.get("/search",searchUserControllers.searchUserBylocationAndTechs);
 
 module.exports = routes;
