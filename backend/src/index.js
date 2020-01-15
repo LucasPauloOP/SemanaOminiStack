04ad797db721e4b269cf5faf79e0e34337a5a137
lucaspauloop <lucaspaulodeoliveira@outlook.com>
2020-01-15 08:48:04 -0300

    Retirando avisos de depreciação

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,11 @@ app.use(express.json());
 app.use(routes);
 
 mongoose.connect("mongodb+srv://LucasPauloDev:adminadmin@cluster0-yt2qw.mongodb.net/OminiStack?retryWrites=true&w=majority",
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
 
 app.listen(3000);
